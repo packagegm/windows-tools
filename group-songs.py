248b6eff29b0676c216ee1afef39c517c16d21f9
packagegm <zhouguangming@outlook.com>
2022-04-07 00:40:36 +0800

    add group songs scripts

---
+++ group-songs.py
@@ -0,0 +1,24 @@
+import os
+from shutil import copyfile
+
+song_file_location = 'F:\\musics\\'
+group_location = song_file_location + '\\group'
+if not os.path.exists(group_location):	
+	os.mkdir(group_location)
+
+print('Numer of files : [' + str(len(os.listdir(song_file_location))) + ']')
+
+for folder_name, subfolders, file_names in os.walk(song_file_location):
+	for file_name in file_names:
+		print('- File name is : [' + file_name + ']')
+		if '-' in file_name:
+			singer_name = file_name.split('-')[0].strip()
+			singer_location = group_location + '\\' + singer_name
+			if not os.path.exists(singer_location):	
+				os.mkdir(singer_location)
+				print('- New singer : [' + singer_name + ']')
+			copyfile(song_file_location + file_name, singer_location + '\\' + file_name)
+			print('- Moved : [' + file_name + '].')
+
+			
+
