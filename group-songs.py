ae170d3f5ea539a9f038cc8646625f845db15421
packagegm <zhouguangming@outlook.com>
2022-04-07 23:27:41 +0800

    update rules

--- group-songs.py
+++ group-songs.py
@@ -11,8 +11,12 @@ print('Numer of files : [' + str(len(os.listdir(song_file_location))) + ']')
 for folder_name, subfolders, file_names in os.walk(song_file_location):
 	for file_name in file_names:
 		print('- File name is : [' + file_name + ']')
-		if '-' in file_name:
+		if  file_name.startswith('['):
+			singer_name = file_name[1 : file_name.find(']')]
+		elif '-' in file_name:
 			singer_name = file_name.split('-')[0].strip()
+		if singer_name is not None:
+			print('- Find singer: [' + singer_name + ']')
 			singer_location = group_location + '\\' + singer_name
 			if not os.path.exists(singer_location):	
 				os.mkdir(singer_location)
