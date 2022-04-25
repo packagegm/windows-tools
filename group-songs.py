73037fd1ba4e31402886727024ecd6b4051adf24
packagegm <zhouguangming@outlook.com>
2022-04-25 20:47:26 +0800

    update

--- group-songs.py
+++ group-songs.py
@@ -15,14 +15,17 @@ for folder_name, subfolders, file_names in os.walk(song_file_location):
 			singer_name = file_name[1 : file_name.find(']')]
 		elif '-' in file_name:
 			singer_name = file_name.split('-')[0].strip()
+		else:
+			singer_name = 'unknown'
 		if singer_name is not None:
 			print('- Find singer: [' + singer_name + ']')
 			singer_location = group_location + '\\' + singer_name
 			if not os.path.exists(singer_location):	
 				os.mkdir(singer_location)
 				print('- New singer : [' + singer_name + ']')
-			copyfile(song_file_location + file_name, singer_location + '\\' + file_name)
-			print('- Moved : [' + file_name + '].')
+			if not os.path.exists(singer_location + '\\' + file_name):
+				copyfile(song_file_location + file_name, singer_location + '\\' + file_name)
+				print('- Moved : [' + file_name + '].')
 
 			
 
